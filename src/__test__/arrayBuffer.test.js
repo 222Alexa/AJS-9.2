65bdb62233d3811584a34e41fdd6d6281e723e28
Alexandra Titova <auto-steklo@mail.ru>
2021-03-10 14:00:56 +0300

    ArrayBuffer

---
+++ src/__test__/arrayBuffer.test.js
@@ -0,0 +1,17 @@
+import getBuffer from '../js/getBuffer';
+import ArrayBufferConverter from '../js/ArrayBufferConverter';
+
+test('load() принимает buffer и сохраняет его в arrayBuffer', () => {
+  const arrayBuffer = new ArrayBufferConverter();
+  const buffer = getBuffer();
+  arrayBuffer.load(buffer);
+  expect(arrayBuffer.bufferContainer).toBeInstanceOf(Uint16Array);
+});
+
+test('toString() корректно раскодирует переданные данные', () => {
+  const dataStr = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+  const arrayBuffer = new ArrayBufferConverter();
+  const buffer = getBuffer();
+  arrayBuffer.load(buffer);
+  expect(arrayBuffer.toString()).toBe(dataStr);
+});
---
+++ src/js/ArrayBufferConverter.js
@@ -0,0 +1,13 @@
+export default class ArrayBufferConverter {
+  constructor() {
+    this.bufferContainer = null;
+  }
+
+  load(data) {
+    this.bufferContainer = new Uint16Array(data);
+  }
+
+  toString() {
+    return [...this.bufferContainer].reduce((acc, prev) => acc + String.fromCharCode(prev), '');
+  }
+}
